69202cdef1cf7fb9d37a42827ef6d8a6b8154fce
Shaundai Person <shaundaiperson@gmail.com>
2019-10-25 21:40:39 -0400

    add event handlers to search bar

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -11,16 +11,14 @@ class SearchBar extends React.Component{
       location: '',
       sortBy: 'best_match'
     };
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+
     this.sortByOptions = {
       'Best Match': "best_match",
       'Highest Rated': "rating",
       'Most Reviewed': "review_count"
   }
-  }
-  handleSortByChange(sortByOption){
-    this.setState({
-      sortBy: {sortByOption}
-})
   }
 
   getSortByClass(sortByOption){
@@ -31,13 +29,32 @@ class SearchBar extends React.Component{
     }
   }
 
+  handleSortByChange(sortByOption){
+    this.setState({
+      sortBy: sortByOption
+})
+  }
+
+  handleTermChange(event){
+    this.setState({
+      term: event.target.value
+    })
+  }
+
+  handleLocationChange(event){
+    this.setState({
+      location: event.target.value
+    })
+  }
+
     renderSortByOptions(){
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
-            return <li className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
+            return <li onClick={this.handleSortByChange.bind(this,sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)} key={sortByOptionValue}>{sortByOption}</li>
         }
         )
     }
+
     render(){
         return (
             <div className="SearchBar">
@@ -47,8 +64,8 @@ class SearchBar extends React.Component{
     </ul>
   </div>
   <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
+    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+    <input onChange={this.handleLocationChange} placeholder="Where?" />
   </div>
   <div className="SearchBar-submit">
     <a>Let's Go</a>
