42b27e3ad05934cf46080c3728ef74ac81ff940f
Shaundai Person <shaundaiperson@gmail.com>
2019-11-03 14:59:21 -0500

    add 1/2 yelp api

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'fVpJrKaOcMREn-lVfSbjtGP-3JzQNwrayg0q8lb4rpkkXcduTsBMbmlr1riWbSuWaFaOraIwRwGZtmYo08V3FQaP82Co-zUSb9JWNi_Z5v2cbvJs-fU3NqRewB6_XXYx';
+
+const Yelp = {
+    search(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+            }).then(response=>{
+                return response.json();
+            }).then(jsonResponse => {
+                if(jsonResponse.businesses){
+                    return jsonResponse.businesses.map(business => {
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.location.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories.title,
+                            rating: business.rating,
+                            reviewCount: business.review_count,
+                        }
+                    })
+                }
+            })
+    }
+}
